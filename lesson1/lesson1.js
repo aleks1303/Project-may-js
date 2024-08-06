bc7796de60de1be22e9cc5df573ad954908a241d
aleks1303 <aleksbulda13@gmail.com>
2024-08-06 13:26:11 +0300

    lesson1.js

---
+++ lesson1/lesson1.js
@@ -0,0 +1,68 @@
+
+
+// task_1---------------
+
+let message = 'hello';
+let str1 = 'owu';
+let srt2 = 'com';
+let country = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bool1 = true;
+let bool2= false;
+
+console.log(message)
+console.log(str1)
+console.log(srt2)
+console.log(country)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+console.log(bool1)
+console.log(bool2)
+
+console.log(message,str1,srt2,country,num1,num2,num3, num4, num5,num6, num7, bool1, bool2)
+
+
+
+
+//task_2------------------
+
+let firstName = 'Bulda';
+let middleName = 'Olexandr';
+let lastName = 'Sergiyovich';
+let person = firstName +
+    ' ' +
+    middleName +
+    ' ' +
+    lastName
+console.log(person)
+
+
+
+// task3--------------------
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+// task4---------------------
+
+let aaa = prompt();
+let bbb = prompt();
+let ccc =   prompt();
+console.log(aaa,bbb,ccc)
