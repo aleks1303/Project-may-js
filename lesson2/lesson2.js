db5d1586a0b28da04520f3bef84fe1d5feca5aa5
aleks1303 <aleksbulda13@gmail.com>
2024-08-08 23:32:44 +0300

    lesson2.js

---
+++ lesson2/lesson2.js
@@ -0,0 +1,156 @@
+
+let array = ['oleg', 'sasha', 'vova', 'petya', 123, 432, 'anna', 'content', '1223', 1.3 ];
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+console.log(array[5])
+console.log(array[6])
+console.log(array[7])
+console.log(array[8])
+console.log(array[9])
+
+
+//----object---------------------
+
+let book1 = {
+    title: 'Around',
+    pageCount: 223,
+    genre: 'fantastic'
+}
+
+let book2 = {
+    title: 'Space',
+    pageCount: 400,
+    genre: 'documental'
+}
+
+let book3 = {
+    title: 'Sun',
+    pageCount: 135,
+    genre: 'discover'
+}
+console.log(book1,book2, book3)
+
+
+// -----------object--------------
+
+let book4 = {
+    title: 'book',
+    pageCount: 254,
+    genre: 'classic',
+    authors: [
+        {
+            name: 'Aleks',
+            age: 31
+        }
+    ]
+}
+let book5 = {
+    title: 'Switch',
+    pageCount: 525,
+    genre: 'roman',
+    authors: [
+        {
+            name: 'Julia',
+            age: 34
+        }
+    ]
+}
+let book6 = {
+    title: 'Cars and driver',
+    pageCount: 345,
+    genre: 'discover',
+    authors: [
+        {
+            name: 'Jason',
+            age: 44
+        }
+    ]
+}
+console.log(book4, book5, book6)
+
+// -----------arrays---------------
+
+let users = [
+    {name: 'Maks', username: 'Jon', password: 234233 },
+    {name: 'Jimmi', username: 'Jonn', password: 343343 },
+    {name: 'Marina', username: 'Jill', password: 456655 },
+    {name: 'Nelya', username: 'Mom', password: 456777 },
+    {name: 'Petya', username: 'Moon', password: 8767656 },
+    {name: 'Vica', username: 'Smil', password: 98465455 },
+    {name: 'Ivan', username: 'Soil', password: 7544567 },
+    {name: 'Dima', username: 'Packet', password: 8665565 },
+    {name: 'Mila', username: 'Villa', password: 76654444 },
+    {name: 'Kristi', username: 'Smitty', password: 77556677 }
+]
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
+
+
+// -------------days-temp-------------
+
+let days = [
+    {
+        day1: {
+            morning:20,
+            afternoon:30,
+            evening:15
+        },
+        day2: {
+            morning:15,
+            afternoon:34,
+            evening:15
+        },
+        day3: {
+            morning:14,
+            afternoon:33,
+            evening:19
+        },
+        day4: {
+            morning:11,
+            afternoon:25,
+            evening:15
+        },
+        day5: {
+            morning:20,
+            afternoon:31,
+            evening:17
+        },
+        day6: {
+            morning:18,
+            afternoon:29,
+            evening:16
+        },
+        day7: {
+            morning:20,
+            afternoon:34,
+            evening:16
+        }
+
+    }
+]
+console.log(days[0].day6.afternoon)
+
+// -------------------------
+
+
+
+
+
+
+
+
+
+
+
+
