f3c69fd75bd53bed37c083b9b012fa8b06357c46
aleks1303 <aleksbulda13@gmail.com>
2024-08-10 23:06:52 +0300

    lesson3.js

---
+++ lesson3/lesson3.js
@@ -0,0 +1,37 @@
+
+
+// --------------for------------
+
+// for (let i = 1; i <= 10; i++){
+//     document.write('<div>Word</div>');
+// }
+//
+// for (let a = 1; a <= 10; a++){
+//     document.write('<div>Word '+ a +'</div>')
+// }
+//
+// // ---------------while---------------
+//
+// let b = 1;
+// while (b <= 20) {
+//     document.write('<h1>the words what we hear</h1>');
+//     b++
+// }
+//
+// let c = 1;
+// while (c <= 20) {
+//     document.write('<h1>the words what we hear '+ c +'</h1>');
+//     c++
+// }
+
+// -------------
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write('<ul>')
+for (let list = 0; list <= listOfItems.length; list++ ){
+    document.write('<li>'+ listOfItems[list] +'</li>');
+}
+document.write('</ul>')
+
+//----------------------------------
+
