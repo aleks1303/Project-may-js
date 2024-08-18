28f8276b98206396bb94789f8e5c89a6051c269e
aleks1303 <aleksbulda13@gmail.com>
2024-08-18 23:44:51 +0300

    lesson5.js

---
+++ lesson6 _array-functions/array-functions.js
@@ -0,0 +1,158 @@
+//
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let words1 = 'hello world'
+let words2 = 'lorem ipsum'
+let words3 = 'javascript is cool'
+
+console.log(words1.length)
+console.log(words2.length)
+console.log(words3.length)
+
+const words4 = ['hello world', 'lorem ipsum', 'javascript is cool']
+for (const item of words4) {
+    console.log(item.length)
+}
+
+// #8lld9HMxXWB
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let word1 = 'hello world'
+console.log(word1.toUpperCase())
+
+let word2 = 'lorem ipsum'
+console.log(word2.toUpperCase())
+
+let word3 = 'javascript is cool'
+console.log(word3.toUpperCase())
+
+
+// #ClDsAm7xba7
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let word4 = 'HELLO WORLD'
+let word5 = 'LOREM IPSUM'
+let word6 = 'JAVASCRIPT IS COOL'
+
+console.log(word4.toLowerCase())
+console.log(word5.toLowerCase())
+console.log(word6.toLowerCase())
+
+// #0b89BkYZwu
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+let str = ' dirty string   '
+let s = str.trim()
+console.log(s)
+
+//     #bfoJuse4ZzP
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+const stringToArray = (str) => {
+    if (str){
+        const srting1 = str.split(' ')
+        return srting1
+    }
+    return [' ']
+}
+console.log(stringToArray());
+
+
+// #Rbr5kEQ
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+let numbers = [10,8,-7,55,987,-1011,0,1050,0]
+let numberMap = numbers.map(number => number.toString());
+
+console.log(numberMap)
+
+
+// #5hqyKTfmc
+// - створити функцію sortNums(array,direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+
+function sortNums (array,direction) {
+
+    if (direction === 'ascending')
+        return array.sort((a, b) => a - b);
+    if (direction === 'descending')
+        return array.sort((a, b) => b - a)
+}
+let nums = [11,21,3]
+console.log(sortNums(nums,'ascending'));
+
+
+// ==========================
+// #yo06d74c1C
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+// -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+// =========================
+
+
+
+
+
+
+
+
+
+//     #bolvdlhP
+// описати колоду карт (від 6 до туза без джокерів)
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king'
+//     color:'', // 'red','black'
+// }
+//
+// =========================
+
+
+
+
+
+
+
+
+// #EP5I1UUzAX
+// Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//         diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+// =========================
+
+
+
+
+
+// #4LJn7zBx
+// взяти з arrays.js масив coursesArray
+// --написати пошук всіх об'єктів, в яких в modules є sass
+// --написати пошук всіх об'єктів, в яких в modules є docker
